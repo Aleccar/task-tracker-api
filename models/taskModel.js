1214fe59b5ac1d074bf36137b4b248b4d5ccae6f
Alec Strand <alexander@strand.it>
2025-06-13 16:02:34 +0200

    fixed a minor bug issue (missing await statement

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,22 +1,20 @@
 const { createClient } = require('@supabase/supabase-js');
-const e = require('express');
-
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY)
 
 
 const getAllTasks = async (filterKey, filterValue) => {
-    if (filterKey, filterValue) {
+    if (filterKey && filterValue) {
         const { data, error } = await supabase
             .from('tasks')
             .select('*')
             .eq(filterKey, filterValue)
 
         return { data, error }
-    } else {
-        const { data, error } = supabase.from('tasks').select('*')
-        return { data, error }
     }
+    
+    const { data, error } = await supabase.from('tasks').select('*')
+    return { data, error }
 };
 
 
