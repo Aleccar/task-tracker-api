d35bd61a63d9c83d4223e7f878041cff1a899213
Alec Strand <alexander@strand.it>
2025-06-17 15:58:23 +0200

    Added loginRouter and a route from app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const taskRouter = require('./routes/taskRouter')
+const loginRouter = require('./routes/loginRouter')
 const morgan = require('morgan')
 const cors = require('cors')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 
 app.use('/tasks', taskRouter)
+app.use('/login', loginRouter)
 
 
 
---
+++ routes/loginRouter.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+
+const loginRouter = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = loginRouter
